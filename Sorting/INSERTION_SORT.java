394dc9650054545037d62ff69d43f516c28380c6
Anish Shrestha <100459851+shresthanish@users.noreply.github.com>
2022-06-06 00:33:55 -0400

    CLRS Algorithms implemented in Java

---
+++ Sorting/INSERTION_SORT.java
@@ -0,0 +1,23 @@
+class SORTING {
+	public static void main(String[] args) {
+		int A[] = {235,4,3,2,41};
+		INSERTION(A);
+		for (int i = 0; i < A.length; i++) {
+			System.out.print(A[i]+" ");
+		}
+		
+	}
+	
+	public static void INSERTION(int A[]){
+		for (int j=1; j<A.length; j++) {
+			int key = A[j];
+			int i = j-1;
+			while (i>=0 && A[i]>key) {
+				A[i+1] = A[i];
+				i = i-1;
+			}
+			A[i+1] = key;
+		}
+	}
+	
+}

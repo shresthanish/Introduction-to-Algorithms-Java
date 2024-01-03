5f77b0f8d1ca9238a4a0f169a1a43d7cf03f02a9
Anish Shrestha <100459851+shresthanish@users.noreply.github.com>
2024-01-03 17:44:31 -0500

    Update Merge_Sort.java

--- Sorting/Merge_Sort.java
+++ Sorting/Merge_Sort.java
@@ -1,53 +1,69 @@
-class Sorting {
-	public static void main(String[] args) {
-		int[] A = { 9, 4, 8, 1, 7, 90, 3, 2, 35, 6};
-		MERGE_SORT(A, 0, A.length-1);
-		
-		for (int i = 0; i < A.length; i++) {
-			System.out.print(A[i]+" ");
-		}
-	}
-	
-	public static void MERGE(int[] A, int p, int q, int r){
-		int n1 = q-p+1;
-		int n2 = r-q;
-		
-		int[] L = new int[n1];
-		int[] R = new int[n2];
-		
-		for (int i = 0; i < n1; i++)
-			L[i] = A[p+i];
-		
-		for (int j = 0; j < n2; j++)
-			R[j] = A[q+j+1];
-		
-		for(int i = 0, j = 0, k = p; k <= r; k++) {
-			if (i == n1) {
-				A[k] = R[j];
-				j=j+1;
-				
-			} else if (j == n2) {
-				A[k] = L[i];
-				i= i+1;
-				
-			} else if (L[i] <= R[j]) {
-				A[k]=L[i]; 
-				i= i+1;
-				
-			} else {
-				A[k]=R[j];
-				j= j+1;
-			}
-		}
-	}
-	
-	public static void MERGE_SORT(int[] A, int p, int r){
-		if (p<r) {
-			int q = (p+r)/2;
-			MERGE_SORT(A, p, q);
-			MERGE_SORT(A, q+1, r);
-			MERGE(A, p, q, r);
-		}
-	}
-	
+public class Merge_Sort {
+    public static void MERGE(int[] A, int p, int q, int r) {
+        int nL = q - p + 1;
+        int nR = r - q;
+
+        int[] L = new int[nL];
+        int[] R = new int[nR];
+
+        for (int i = 0; i < nL; ++i)
+            L[i] = A[p + i];
+        for (int j = 0; j < nR; ++j)
+            R[j] = A[q + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = p;
+        while (i < nL && j < nR) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i = i + 1;
+            } else {
+                A[k] = R[j];
+                j = j + 1;
+            }
+            k = k + 1;
+        }
+
+        while (i < nL) {
+            A[k] = L[i];
+            i = i + 1;
+            k = k + 1;
+        }
+
+        while (j < nR) {
+            A[k] = R[j];
+            j = j + 1;
+            k = k + 1;
+        }
+    }
+
+    public static void MERGE_SORT(int[] A, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            MERGE_SORT(A, p, q);
+            MERGE_SORT(A, q + 1, r);
+            MERGE(A, p, q, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] A = { 9, 4, 8, 1, 7, 90, 3, 2, 35, 6 };
+        System.out.print("Intput: ");
+        print(A);
+        MERGE_SORT(A, 0, A.length - 1);
+        System.out.print("Output: ");
+        print(A);
+    }
+
+    public static void print(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            System.out.printf("%4d", A[i]);
+            if (i != A.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+    }
+
 }

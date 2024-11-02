5f84b4e49015de69a613e92ade1d062fbcc7aeef
Anish Shrestha <100459851+shresthanish@users.noreply.github.com>
2024-11-02 03:15:46 -0500

    Update Stack.java

--- Elementary Data Structures/Stack/Array/Stack.java
+++ Elementary Data Structures/Stack/Array/Stack.java
@@ -1,14 +1,37 @@
-public interface Stack{
-	
-	//Pushes an object onto the stack.
-	public void push(Object x);
-	
-	//Pops an object from the stack, returining the popped object.
-	public Object pop();
-	
-	//Returns true if the stack is empty, false otherwise.
-	public boolean isEmpty();
-	
-	//prints the content of the stack. 
-	public void display();
+// Stack.java
+public class Stack {
+    private int[] array;
+    private int top;
+    private int size;
+
+    // Constructor
+    public Stack(int n) {
+        array = new int[n];
+        this.size = n;
+        top = -1;  // Initialize top to -1 to indicate empty stack
+    }
+
+    // STACK-EMPTY(S)
+    public static boolean STACK_EMPTY(Stack S) {
+        if (S.top == -1)
+            return true;
+        else
+            return false;
+    }
+
+    // PUSH(S,x)
+    public static void PUSH(Stack S, int x) {
+        if (S.top == S.size - 1)
+            throw new RuntimeException("overflow");
+        S.top = S.top + 1;
+        S.array[S.top] = x;
+    }
+
+    // POP(S)
+    public static int POP(Stack S) {
+        if (STACK_EMPTY(S))
+            throw new RuntimeException("underflow");
+        S.top = S.top - 1;
+        return S.array[S.top + 1];
+    }
 }
